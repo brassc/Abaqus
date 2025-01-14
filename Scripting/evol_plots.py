0782e67b3856e784b5ee14796b504ef45f5f1ce9
C.M. Brass <cmb247@cam.ac.uk>
2025-01-14 13:54:39 +0000

    remove 2mm mentions

--- Scripting/evol_plots.py
+++ Scripting/evol_plots.py
@@ -23,8 +23,8 @@ evol_10mm=pd.read_csv(evol_10mm_path)
 #evol_3mm=evol_5mm.copy()
 #evol_3mm['EVOL Sum']=0.5*evol_5mm['EVOL Sum']
 
-evol_2mm=evol_5mm.copy()
-evol_2mm['EVOL Sum']=1.05*evol_5mm['EVOL Sum']
+#evol_2mm=evol_5mm.copy()
+#evol_2mm['EVOL Sum']=1.05*evol_5mm['EVOL Sum']
 
 #evol_10mm=evol_5mm.copy()
 #evol_10mm['EVOL Sum']=1.1*evol_5mm['EVOL Sum']
@@ -36,20 +36,15 @@ evol_2mm['EVOL Sum']=1.05*evol_5mm['EVOL Sum']
 # Find peak EVOL 
 peak_5mm_evol=evol_5mm['EVOL Sum'].max()
 peak_3mm_evol=evol_3mm['EVOL Sum'].max()
-peak_2mm_evol=evol_2mm['EVOL Sum'].max()
+#peak_2mm_evol=evol_2mm['EVOL Sum'].max()
 peak_10mm_evol=evol_10mm['EVOL Sum'].max()
-"""
-#fake data for testing
-peak_3mm_evol=0.5*peak_5mm_evol
-peak_2mm_evol=0.25*peak_5mm_evol
-peak_10mm_evol=1.1*peak_5mm_evol
-"""
+
 # Mesh sizes
-mesh_sizes=[2, 3, 5, 10]
+mesh_sizes=[3, 5, 10]
 
 # plotting peak EVOL vs mesh size
 plt.figure(figsize=(10,6))
-plt.scatter(mesh_sizes, [peak_2mm_evol, peak_3mm_evol, peak_5mm_evol, peak_10mm_evol], color='b', label='Peak EVOL')
+plt.scatter(mesh_sizes, [peak_3mm_evol, peak_5mm_evol, peak_10mm_evol], color='b', label='Peak EVOL')
 plt.xlabel('Mesh size (mm)')
 plt.ylabel('Peak Cerebrum Volume (mm³)')
 plt.xlim(0)
@@ -61,7 +56,7 @@ plt.show()
 
 # plotting EVOL vs time for each mesh
 plt.figure(figsize=(10,6))
-plt.plot(evol_2mm['Frame'], evol_2mm['EVOL Sum'], color='r', label='2mm')
+#plt.plot(evol_2mm['Frame'], evol_2mm['EVOL Sum'], color='r', label='2mm')
 plt.plot(evol_3mm['Frame'], evol_3mm['EVOL Sum'], color='g', label='3mm')
 plt.plot(evol_5mm['Frame'], evol_5mm['EVOL Sum'], color='b', label='5mm')
 plt.plot(evol_10mm['Frame'], evol_10mm['EVOL Sum'], color='y', label='10mm')
