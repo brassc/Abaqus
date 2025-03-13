1fbe8b0da503c1f01e0750062886ce29bf8475a6
brassc <cmb247@cam.ac.uk>
2025-03-13 19:24:26 +0000

    add figure formatting function

--- Scripting/Thesis_Extractions/odb_extract_plot.py
+++ Scripting/Thesis_Extractions/odb_extract_plot.py
@@ -8,8 +8,32 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from mpl_toolkits.mplot3d import Axes3D
 
+def set_publication_style():
+	"""Set matplotlib parameters for publication-quality figures."""
+	plt.rcParams.update({
+		'font.family': 'serif',
+		'font.serif': ['Times New Roman'],
+		'mathtext.fontset': 'stix',
+		'font.size': 12,
+		'axes.labelsize': 14,
+		'axes.titlesize': 16,
+		'axes.titleweight': 'bold', # This makes titles bold
+		'xtick.labelsize': 12,
+		'ytick.labelsize': 12,
+		'legend.fontsize': 10,
+		'figure.dpi': 150,
+		'savefig.dpi': 300,
+		'savefig.format': 'png',
+		'savefig.bbox': 'tight',
+		'axes.grid': True,
+		'grid.alpha': 0.3,
+		'grid.linestyle': '-',
+		'axes.spines.top': False,
+		'axes.spines.right': False,
+	})
 
-
+# for plots
+set_publication_style()
 
 # Pickle basepath
 print("running...")
