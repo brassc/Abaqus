aee5472f797958a5a489e9ec571dbef603f5d05a
brassc <cmb247@cam.ac.uk>
2025-04-24 14:40:37 +0100

    add publication style to evol_plots.py

--- Scripting/evol_plots.py
+++ Scripting/evol_plots.py
@@ -3,6 +3,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 
+def set_publication_style():
+	"""Set matplotlib parameters for publication-quality figures."""
+	plt.rcParams.update({
+		'font.family': 'serif',
+		'font.serif': ['Times New Roman'],
+		'mathtext.fontset': 'stix',
+		'font.size': 12,
+		'axes.labelsize': 14,
+		'axes.titlesize': 16,
+		'axes.titleweight': 'bold', # This makes titles bold
+		'xtick.labelsize': 12,
+		'ytick.labelsize': 12,
+		'legend.fontsize': 10,
+		'figure.dpi': 150,
+		'savefig.dpi': 300,
+		'savefig.format': 'png',
+		'savefig.bbox': 'tight',
+		'axes.grid': True,
+		'grid.alpha': 0.3,
+		'grid.linestyle': '-',
+		'axes.spines.top': False,
+		'axes.spines.right': False,
+	})
+
 # Define a base path
 base_path=Path.cwd()
 print(base_path)
